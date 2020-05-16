1d513ef89b2c931c4b0a13c60ed138cc43a809b6
Ariel Perez <arielperezch@gmail.com>
2020-05-15 21:33:32 -0700

    Added main within src to try whether this works on Cloud repo

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from flask import escape
+
+
+# [START functions_helloworld_http]
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
+# [END functions_helloworld_http]
