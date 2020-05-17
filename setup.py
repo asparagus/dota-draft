306f538eef40f216f6715caaae509e038993ac6f
Ariel Perez <arielperezch@gmail.com>
2020-05-17 15:58:04 -0700

    Removed unused code

--- setup.py
+++ setup.py
@@ -19,6 +19,4 @@ setuptools.setup(
     url='https://github.com/Ariel-Perez/dota-draft',
     install_requires=requirements,
     packages=['draft'],  # Omit test package
-    # packages=setuptools.find_packages("src"),  # include all packages under src
-    # package_dir={"": "src"}                    # tell distutils packages are under src
 )
