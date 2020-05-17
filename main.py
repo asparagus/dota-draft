1e200e881a6f89889953421e23f64538a7aba1f2
Ariel Perez <arielperezch@gmail.com>
2020-05-16 23:04:16 -0700

    Added comment with deploy command for cloud function

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+"""Cloud funtions entry point.
+
+Cloud functions defined in this file:
+- collect
+    Requests new match ids to the OpenDota API and saves them to cloud storage.
+
+To deploy cloud functions run the following command from the root of the repository.
+
+gcloud functions deploy collect \
+    --entry-point=collect \
+    --runtime=python37 \
+    --trigger-http \
+    --project=dota-draft \
+    --source=. \
+    --ingress-settings=internal-only \
+    --set-env-vars=<DOTA_API_KEY>
+
+Check additional documentation at https://cloud.google.com/sdk/gcloud/reference/functions/deploy.
+"""
 from src import collect as coll
 
 
