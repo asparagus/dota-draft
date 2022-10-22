c800a6f67e4826612d5fc674767a5e7cd7b40e22
asparagus <arielperezch@gmail.com>
2022-10-22 03:31:10 -0700

    Add tests ensuring model is symmetric by construction when option is passed

---
+++ tests/model/test_simplemodel.py
@@ -0,0 +1,24 @@
+import numpy as np
+import torch
+
+from draft.model import simplemodel
+
+
+def test_symmetry():
+    symmetric_config = simplemodel.SimpleModelConfig(
+        num_heroes=25,
+        dimensions=(1,),
+        symmetric=True,
+    )
+    regular_config = simplemodel.SimpleModelConfig(
+        num_heroes=25,
+        dimensions=(1,),
+        symmetric=False,
+    )
+    symmetric_model = simplemodel.SimpleModel(symmetric_config)
+    regular_model = simplemodel.SimpleModel(regular_config)
+    draft = torch.tensor([[1,2,3,4,5,6,7,8,9,10]], dtype=torch.long)
+    opposite_draft = torch.tensor([[6,7,8,9,10,1,2,3,4,5]], dtype=torch.long)
+    symmetric_result = symmetric_model(draft)
+    opposite_result = symmetric_model(opposite_draft)
+    torch.testing.assert_close(symmetric_result, 1 - opposite_result)
