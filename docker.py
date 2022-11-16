86153818ec04cd1ada7ba388f6ea28f5d05bfd33
asparagus <arielperezch@gmail.com>
2022-11-15 21:53:45 -0800

    Push only compact-worker image to GAR, save money

--- docker.py
+++ docker.py
@@ -17,5 +17,6 @@ ENVIRONMENTS = [
 if __name__ == '__main__':
     for env in ENVIRONMENTS:
         os.system(f'docker build -t {env}:latest . -f environments/{env}/Dockerfile')
-        os.system(f'docker tag {env} {GAR.location}-docker.pkg.dev/{GAR.project}/{GAR.repository}/{env}:latest')
-        os.system(f'docker push {GAR.location}-docker.pkg.dev/{GAR.project}/{GAR.repository}/{env}:latest')
+        if env == 'compact-worker':
+            os.system(f'docker tag {env} {GAR.location}-docker.pkg.dev/{GAR.project}/{GAR.repository}/{env}:latest')
+            os.system(f'docker push {GAR.location}-docker.pkg.dev/{GAR.project}/{GAR.repository}/{env}:latest')
